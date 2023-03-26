fd0d67ef17b7bbea892badaaa4454172b7994c26
anh-nguyen-katalon <anh.hnnguyen@katalon.com>
2023-03-26 16:09:54 +0700

    add window_handle

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ options = ChromeOptions()
 driver = webdriver.Chrome(options=options)
 
 # set timeout
-driver.implicitly_wait(5)
+driver.implicitly_wait(10)
 
 driver.get('https://testops.staging.katalon.com')
 
 title = driver.title
 assert title == 'Katalon TestOps'
 
-# fill username
+print (driver.window_handles)
+# fill username 
 username = driver.find_element(by=By.CSS_SELECTOR, value='[name=username]')
 username.send_keys('hai.nguyen+4@katalon.com')
 # fill password
@@ -27,9 +28,27 @@ password.send_keys('-9gBFiixf8H2wXg')
 # click submit button
 submit_button = driver.find_element(by=By.CLASS_NAME, value='signin-button')
 submit_button.click()
+
 # find logo link to homepage
 logo_link = driver.find_element(by=By.CLASS_NAME, value='navbar-brand')
 assert logo_link.is_displayed
 
+driver.get('https://www.selenium.dev/documentation/')
+assert "Automation" in driver.title 
+print (driver.window_handles)
+
+edit_page_link = driver.find_element(by=By.LINK_TEXT, value='Edit this page')
+edit_page_link.click()
+
+print(driver.window_handles)
+main_tab = driver.current_window_handle
+print ('Before switch:', main_tab)
+
+
+for handle in driver.window_handles:
+    if handle != main_tab:
+        driver.switch_to.window(handle)
+        main_tab = driver.current_window_handle
+print('After switch:', main_tab)
 # end browser session
 driver.quit()
